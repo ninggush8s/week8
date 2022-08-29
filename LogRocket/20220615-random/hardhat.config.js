ff4f67e77229860d505b9a23b044100a2c4cff6c
ninggush8s <juliosmorrine@gmail.com>
2022-08-29 07:00:41 +0000

    ffweek8

--- LogRocket/20220615-random/hardhat.config.js
+++ LogRocket/20220615-random/hardhat.config.js
@@ -11,33 +11,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-
 module.exports = {
   solidity: "0.8.4",
-/*
+
   networks: {
-    "local-devnode": {
-       url: "http://localhost:8545",
-       accounts: { mnemonic: "test test test test test test test test test test test junk" }
-    },
-    "optimistic-kovan": {
-       url: "https://kovan.optimism.io",
-       accounts: { mnemonic: process.env.MNEMONIC }
-    },
-    "optimism": {
-       url: "https://mainnet.optimism.io",
-       accounts: { mnemonic: process.env.MNEMONIC }
+    "goerli": {
+       url: "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
+       accounts: ["0x459af5f86a94bce134098b0e1b3e975ff0307ce06978a10aa9fd14fa9e0ee84e"]
     }
   }
-*/
+
 };
